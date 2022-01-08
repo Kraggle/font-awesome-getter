6fc0381fc1a3bb7f4ef4574485b69cfa9280a9d0
Kraggle <kraggle27@gmail.com>
2022-01-08 17:10:17 +0000

    alias update

--- index.php
+++ index.php
@@ -1,36 +1,52 @@
 <?php
 
-$type  = $_REQUEST['type']  ?? $_REQUEST['t'] ?? false;
-$icon  = $_REQUEST['icon']  ?? $_REQUEST['i'] ?? false;
+$file = './metadata/aliases.json';
+if (!file_exists($file)) {
+	$aliases = (object) [];
+	$icons = json_decode(file_get_contents('./metadata/icons.json'));
+	foreach ($icons as $name => $value) {
+		if (isset($value->aliases->names)) {
+			foreach ($value->aliases->names as $alias)
+				$aliases->$alias = $name;
+		}
+	}
+
+	file_put_contents($file, json_encode($aliases, JSON_PRETTY_PRINT));
+}
+
+$type  = $_REQUEST['type']  ?? $_REQUEST['t'] ?? 'regular';
+$icon  = $_REQUEST['icon']  ?? $_REQUEST['i'] ?? 'n932f9ibf78fsa';
 $color = $_REQUEST['color'] ?? $_REQUEST['c'] ?? false;
 
-if ($type && $icon) {
-
-	$path = __DIR__ . "/svgs/$type/$icon.svg";
-
-	if (!file_exists($path)) {
-		echo json_encode([
-			'path' => $path,
-			'error' => 'The icon you requested does not exist!',
-			'success' => false
-		]);
-		exit;
-	}
-
-	$svg = file_get_contents($path);
-	$style = '';
-
-	if ($color) {
-		$id = '_' . uniqid();
-		$svg = str_replace('<svg ', "<svg id=\"$id\" ", $svg);
-
-		$style = "<style>#$id * {fill:$color;}</style>";
-	}
-
-	echo $style . $svg;
-} else {
+if (!file_exists(__DIR__ . "/svgs/$type/")) {
 	echo json_encode([
-		'error' => 'To use this you must add what FontAwesome `type` (e.g. brands, light, solid) and `icon` (e.g. alien, hands, shapes) you want!',
+		'type' => $type,
+		'error' => "That icon type does not exist. The options are 'brands', 'duotone', 'light', 'regular', 'solid' and 'thin'",
 		'success' => false
 	]);
+	exit;
 }
+
+$aliases = json_decode(file_get_contents($file));
+if (isset($aliases->$icon)) $icon = $aliases->$icon;
+$path = __DIR__ . "/svgs/$type/$icon.svg";
+
+if (!file_exists($path)) {
+	echo json_encode([
+		'error' => "The icon you requested does not exist!",
+		'success' => false
+	]);
+	exit;
+}
+
+$svg = file_get_contents($path);
+$style = '';
+
+if ($color) {
+	$id = '_' . uniqid();
+	$svg = str_replace('<svg ', "<svg id=\"$id\" ", $svg);
+
+	$style = "<style>#$id * {fill:$color;}</style>";
+}
+
+echo $style . $svg;
